082cc4caff229a53e9d1ca72742d9fcb6eaef989
rafbo26 <rafaell.borkowski@gmail.com>
2019-07-01 17:48:40 +0100

    Add completed tasks and cheatsheet

--- intro_to_programming.rb
+++ intro_to_programming.rb
@@ -464,3 +464,68 @@ arr_2 = [
 
 print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
 puts
+
+
+# Hash exercises
+
+def get_double_age(hash)
+	return hash["age"] * 2
+end
+
+puts get_double_age({"name"=>"App Academy", "age"=>5}) # => 10
+puts get_double_age({"name"=>"Ruby", "age"=>23})       # => 46
+
+
+def get_full_name(hash)
+	return hash["first"] + " " + hash["last"] + ", the " + hash["title"]
+end
+
+hash1 = {"first"=>"Michael", "last"=>"Jordan", "title"=> "GOAT"}
+puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
+
+hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
+puts get_full_name(hash2) # => "Fido McDog, the Loyal"
+
+
+
+def word_lengths(sentence)
+  hash = {}
+  sent = sentence.split(" ")
+  sent.each do |ele|
+    hash[ele] = ele.length
+  end
+  return hash
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
+
+
+def retrieve_values(hash1, hash2, key)
+  return [hash1[key], hash2[key]]
+end
+
+
+dog1 = {"name"=>"Fido", "color"=>"brown"}
+dog2 = {"name"=>"Spot", "color"=> "white"}
+print retrieve_values(dog1, dog2, "name") #=> ["Fido", "Spot"]
+puts
+print retrieve_values(dog1, dog2, "color") #=> ["brown", "white"]
+puts
+
+
+
+def cat_builder(name_str, color_str, age_num)
+  hash = {
+    "name" => name_str,
+    "color" => color_str,
+    "age_num" => age_num
+  }
+  return hash
+end
+
+print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
+puts
+
+print cat_builder("Salem", "black", 100) #=> {"name"=>"Salem", "color"=>"black", "age"=>100}
+puts
