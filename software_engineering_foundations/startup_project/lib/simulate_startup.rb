504365479e3e2b8872e8a70fa37339aeb0aaf9d7
Rafal Borkowski <rafaell.borkowski@gmail.com>
2019-07-10 13:24:16 +0000

    Add completed exercises

---
+++ software_engineering_foundations/startup_project/lib/simulate_startup.rb
@@ -0,0 +1,23 @@
+require_relative "employee"
+require_relative "startup"
+
+# print "Enter a name of your startup: "
+# name = gets.chomp
+# print "Enter funding of your startup: "
+# funding = gets.chomp
+# salaries = {}
+
+# next_title = true
+
+# while true
+#   print "Add new title: "
+#   title_name = gets.chomp
+#   print "Add salary for #{title_name}: "
+#   title_salary = gets.chomp
+#   salaries[title_name] = title_salary
+#   print "Add new title? (y/n): "
+#   input = gets.chomp
+#   if input.downcase == "n"
+#     break
+#   end
+# end
