32b43b34b997b73047609bd71178052af20eed70
rafbo26 <rafaell.borkowski@gmail.com>
2019-07-12 19:28:14 +0100

    Add completed exercises

--- battleship_project/lib/player.rb
+++
@@ -1,8 +0,0 @@
-class Player
-
-  def get_move
-    print "enter a position with coordinates separated with a space like `4 7` "
-    coords = gets.chomp.split(" ")
-    coords.map { |ele| ele.to_i }
-  end
-end
