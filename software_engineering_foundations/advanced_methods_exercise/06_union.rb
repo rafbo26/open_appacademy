8add38bf1c1a771931b75251ba228fa761f5ae5d
rafbo26 <rafbo26@github.com>
2021-04-19 19:03:02 +0100

    Add new exercises

--- software_engineering_foundations/advanced_methods_exercise/06_union.rb
+++ software_engineering_foundations/advanced_methods_exercise/06_union.rb
@@ -1,6 +1,13 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 
+def union(*args)
+  
+  array = []
+  args.each { |arr| array = array + arr }
+  return array
+
+end
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
